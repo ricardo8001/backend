e04f05d3675e4fbf7176d073fdf55310807f03d5
ricardo8001 <ricardo800tr@gmail.com>
2025-07-28 06:35:15 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $saldo = 9999;
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-  
+    <title>AMAZON.COM | Checker AMAZON</title>
     <link rel="stylesheet" href="./assets/css/vendors_css.css">
     <link rel="stylesheet" href="./assets/css/style.css">
     <link rel="stylesheet" href="./assets/css/skin_color.css">
